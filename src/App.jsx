5ed7125727478833eceaefd93edf34a8d86d9da3
young <azerosong@gmail.com>
2024-06-12 00:54:31 +0900

    로그인 회원가입 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
-import Login from './components/Login';
+import './App.css';
+import Login_page from './pages/Login_page';
+import Signup_page from './pages/Signup_page';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      <Login></Login>
-    </>
+    <Routes>
+      <Route path='/login' element={<Login_page />}></Route>
+      <Route path='/signup' element={<Signup_page />}></Route>
+    </Routes>
   )
 }
 
