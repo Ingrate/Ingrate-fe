3c6dfca149129614f951a0a853f471930059821d
young <azerosong@gmail.com>
2024-06-12 00:53:55 +0900

    회원가입 UI 구현

---
+++ src/components/Signup.jsx
@@ -0,0 +1,23 @@
+import "./Signup.css"
+import { NavLink } from "react-router-dom";
+
+function Signup() {
+  return (
+    <div className="Signup">
+      <div className="signup-wrapper">
+        <div className="img"></div>
+        <h1>회원가입</h1>
+        <input type="text" placeholder="사용자명" />
+        <input type="text" placeholder="비밀번호" />
+        <input type="text" placeholder="비밀번호 확인" />
+        <button>회원가입</button>
+        <div className="to-login">
+          <NavLink to="/login">로그인 페이지로 돌아가기</NavLink>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Signup;
