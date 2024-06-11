768a1c731804febb2032525852f9bd8b70bd03af
young <azerosong@gmail.com>
2024-06-12 00:53:38 +0900

    로그인 페이지 구현

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,24 @@
 import "./Login.css"
+import { NavLink } from "react-router-dom";
 
 function Login() {
   return (
     <div className="Login">
       <div className="login-wrapper">
         <div className="img"></div>
-        <h1>Welcome to Ingrate!</h1>
-        <input type="text" placeholder="Username" />
-        <input type="text" placeholder="Password" />
-        <button>Sign in</button>
+        <div className="title">
+          <h1>인그레이트!</h1>
+          <h3>당신의 장보기 파트너</h3>
+        </div>
+        <input type="text" placeholder="사용자명" />
+        <input type="text" placeholder="비밀번호" />
+        <button>로그인</button>
         <div className="keep-signed">
           <input type="checkbox" />
-          <div>Keep me signed in</div>
+          <div>자동 로그인</div>
+        </div>
+        <div className="to-signup">
+          <NavLink to="/signup">아직 계정이 없으신가요?</NavLink>
         </div>
 
       </div>
---
+++ src/pages/Login_page.jsx
@@ -0,0 +1,11 @@
+import Login from "../components/Login";
+
+function Login_page() {
+  return (
+    <>
+      <Login></Login>
+    </>
+  )
+}
+
+export default Login_page;
