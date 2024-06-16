cd71b84b41feade48456e36a2dc2f0de6a25bf20
young <azerosong@gmail.com>
2024-06-16 14:24:45 +0900

    레이팅 페이지에 버튼 생성

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -1,6 +1,18 @@
 import Info from "./Info";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 function Rate({ ingred }) {
+  const nav = useNavigate();
+
+  const onClicktoMain = () => {
+    nav("/main");
+  };
+
+  const onClickRegister = () => {
+    nav("/new");
+  };
+
   return (
     <div className="Rate">
       <div className="info-section">
@@ -9,9 +21,11 @@ function Rate({ ingred }) {
       <div className="rate-section">
         <h3>99점입니다!</h3>
         <div>나쁘지 않은데요?</div>
+        <Button text="메인으로" onClick={onClicktoMain}></Button>
+        <Button text="등록하기" onClick={onClickRegister}></Button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Rate;
