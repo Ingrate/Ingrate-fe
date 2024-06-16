5d4d42820d57c3d27d4afbb2723f2fa62d4e263e
young <azerosong@gmail.com>
2024-06-16 17:37:20 +0900

    등록하기(save) 라우팅 설정

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -5,14 +5,6 @@ import { useNavigate } from "react-router-dom";
 function Rate({ ingred }) {
   const nav = useNavigate();
 
-  const onClicktoMain = () => {
-    nav("/main");
-  };
-
-  const onClickRegister = () => {
-    nav("/new");
-  };
-
   return (
     <div className="Rate">
       <div className="info-section">
@@ -21,8 +13,18 @@ function Rate({ ingred }) {
       <div className="rate-section">
         <h3>99점입니다!</h3>
         <div>나쁘지 않은데요?</div>
-        <Button text="메인으로" onClick={onClicktoMain}></Button>
-        <Button text="등록하기" onClick={onClickRegister}></Button>
+        <Button
+          text="메인으로"
+          onClick={() => {
+            nav("/main");
+          }}
+        ></Button>
+        <Button
+          text="등록하기"
+          onClick={() => {
+            nav("/save");
+          }}
+        ></Button>
       </div>
     </div>
   );
