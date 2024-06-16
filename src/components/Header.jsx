d825482ae25fc40fd815317dace38b5beead27c2
young <azerosong@gmail.com>
2024-06-16 14:48:01 +0900

    헤더 클릭 시 메인으로 설정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 // import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const nav = useNavigate();
   return (
     <div className="Header">
-      <h1 className="text-header mb-3 mt-20 p-8 text-center font-bold">
+      <h1
+        className="text-header mb-3 mt-20 cursor-pointer p-8 text-center font-bold"
+        onClick={() => {
+          nav("/main");
+        }}
+      >
         Ingrate
       </h1>
     </div>
