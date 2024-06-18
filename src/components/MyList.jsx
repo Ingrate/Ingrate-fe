8b268be8417c52881f039ecefb906a174dd3324d
young <azerosong@gmail.com>
2024-06-18 15:44:31 +0900

    리스트 클릭하면 모달 생성

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 function MyList({ id, name, amount, unit, cost }) {
-  console.log(name);
-  console.log(amount);
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <div className="MyList rounded-4xl m-auto mt-5 grid w-11/12 grid-cols-3 border-2 p-5 pl-10 pr-0 text-2xl">
-      <div>{name}</div>
-      <div>
-        {amount}
-        {unit}
+    <div>
+      <div className={`flex justify-center ${clicked ? "" : "hidden"}`}>
+        <div className="rounded-4xl absolute z-10 mt-5 h-96 w-11/12 bg-white"></div>
+        <div
+          className={`absolute left-0 top-0 h-screen w-screen bg-black opacity-30`}
+        ></div>
       </div>
-      <div>{cost}원</div>
+      <div
+        className={`rounded-4xl m-auto mt-5 grid w-11/12 grid-cols-3 border-2 p-5 pl-10 pr-0 text-2xl ${clicked ? "border-amber-400 bg-amber-300" : "bg-transparent"}`}
+        onClick={() => {
+          setClicked(true);
+        }}
+      >
+        <div>{name}</div>
+        <div>
+          {amount}
+          {unit}
+        </div>
+        <div>{cost}원</div>
+      </div>
     </div>
   );
 }
