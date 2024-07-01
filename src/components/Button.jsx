c53ee92022cfa8fc78babe0101fdfa4a46745e49
young <azerosong@gmail.com>
2024-07-01 16:05:16 +0900

    버튼 UI 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ text, color, onClick }) {
   return (
     <div className="Button">
       <button
-        className={`w-36 rounded-lg bg-gray-200 p-4 text-lg`}
+        className={`w-36 rounded-md border-2 border-gray-300 bg-gray-200 pb-3 pt-3 text-lg`}
         onClick={onClick}
       >
         {text}
