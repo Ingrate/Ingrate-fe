248813de883dec645a93b062ea9de9d578e3a103
young <azerosong@gmail.com>
2024-07-05 00:31:25 +0900

    로그인 api 연결: 로그인 페이지에 props 추가

--- src/pages/Login_page.jsx
+++ src/pages/Login_page.jsx
@@ -1,13 +1,18 @@
 import Slogan from "../components/Slogan";
 import Login from "../components/Login";
 
-function Login_page({ login, onChange, onClick }) {
+function Login_page({ login, onChange, onClick, postAuthLogin }) {
   return (
     <div className="m-auto mt-20 flex w-3/4 flex-col">
       <Slogan></Slogan>
       <div className="flex flex-wrap justify-between gap-20 align-middle">
         <div className="m-0 aspect-square w-600 bg-black text-white">로고</div>
-        <Login {...login} onChange={onChange} onClick={onClick}></Login>
+        <Login
+          {...login}
+          onChange={onChange}
+          onClick={onClick}
+          postAuthLogin={postAuthLogin}
+        ></Login>
       </div>
     </div>
   );
