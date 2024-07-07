739094ba18aaa5309c0a144a99e4e2c433941472
young <azerosong@gmail.com>
2024-07-07 17:58:17 +0900

    feat: HTTPS 보안 설정
    - CORS 오류로 https 보안 설정
    - 토큰 방식 변경으로 https 사용하지 않게 되어 주석 처리함

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createServer } from "vite";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+// import.meta.url을 파일 경로로 변환
+const __filename = fileURLToPath(import.meta.url);
+// 파일 경로에서 디렉터리 경로 추출
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // server: {
+  //   https: {
+  //     key: readFileSync(resolve(__dirname, "./localhost+2-key.pem")),
+  //     cert: readFileSync(resolve(__dirname, "./localhost+2.pem")),
+  //   },
+  // },
+});
